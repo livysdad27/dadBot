087c1bcbc5f1841c63a96c87b2e832ead9e9d0da
livysdad27 <livysdad27@gmail.com>
2017-10-24 14:21:24 +0000

    try again

--- index.py
+++ index.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 from chatterbot import ChatBot
-from slackbot.bot import respond_to
-from slackbot.bot import listen_to
-import re
-from slackbot.bot import Bot
-
+from slackclient import SlackClient
+import os
+import time
+import json
 
 # Uncomment the following lines to enable verbose logging
-# import logging
-# logging.basicConfig(level=logging.INFO)
+#import logging
+#logging.basicConfig(level=logging.INFO)
 
 # Create a new instance of a ChatBot
 botBrain = ChatBot(
-    "Terminal",
+    "dadBot",
     storage_adapter="chatterbot.storage.SQLStorageAdapter",
     #logic_adapters=[
     #    "chatterbot.logic.MathematicalEvaluation",
@@ -20,8 +19,6 @@ botBrain = ChatBot(
     #    "chatterbot.logic.BestMatch"
     #],
     trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
-    input_adapter="chatterbot.input.TerminalAdapter",
-    output_adapter="chatterbot.output.TerminalAdapter",
     database="./db/database.db"
 )
 
@@ -32,15 +29,21 @@ botBrain.train(
 )
 
 
-@respond_to('(.*)')
-def invoke(message, theySaid):
-    message.reply(botBrain.get_response(theySaid))
-    # react with thumb up emoji
-    message.react('+1')
+slack_token = os.environ["SLACK_API_TOKEN"]
+sc = SlackClient(slack_token)
 
-def main():
-    bot = Bot()
-    bot.run()
-
-if __name__ == "__main__":
-    main()
+if sc.rtm_connect():
+    while True:
+        evt = sc.rtm_read()
+        if evt != []:
+          print(evt)
+          if 'type' in evt[0]:
+            if evt[0]['type'] == 'message':
+              if evt[0]['user'] != 'U7MKWLER1':
+                print(evt[0]['text'])
+                answer = botBrain.get_response(evt[0]['text'])
+                print(answer)
+                sc.rtm_send_message('D7NJRHWVC', str(answer))
+          time.sleep(1)
+else:
+    print("Connection Failed")
